1c91077d87f3783265be520917a6dae6f0e9c742
yyn9704@gmail.com <yyn9704@gmail.com>
2022-07-29 16:40:14 +0900

    add navigation history fragment and actions

--- app/src/main/java/com/github/yeeun_yun97/toy/mobymovie/ui/MainFragment.kt
+++ app/src/main/java/com/github/yeeun_yun97/toy/mobymovie/ui/MainFragment.kt
@@ -1,7 +1,8 @@
 package com.github.yeeun_yun97.toy.mobymovie.ui
 
 import android.view.View
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.yeeun_yun97.clone.ynmodule.ui.fragment.DataBindingBasicFragment
 import com.github.yeeun_yun97.toy.mobymovie.R
@@ -10,7 +11,7 @@ import com.github.yeeun_yun97.toy.mobymovie.ui.adapter.MovieRecyclerAdapter
 import com.github.yeeun_yun97.toy.mobymovie.viewModel.SearchViewModel
 
 class MainFragment : DataBindingBasicFragment<FragmentMainBinding>() {
-    private val viewModel: SearchViewModel by viewModels()
+    private val viewModel: SearchViewModel by activityViewModels()
 
     override fun layoutId(): Int = R.layout.fragment_main
     override fun onCreateView() {
@@ -38,7 +39,9 @@ class MainFragment : DataBindingBasicFragment<FragmentMainBinding>() {
         viewModel.searchStart()
     }
 
-    private fun navigateToHistory() {}
+    private fun navigateToHistory() {
+        findNavController().navigate(R.id.action_mainFragment_to_historyFragment)
+    }
 
 
 }

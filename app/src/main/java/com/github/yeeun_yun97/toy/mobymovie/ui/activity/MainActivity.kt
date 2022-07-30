f95ed207b4b30702c53d0ac6798506f151ed6d81
yyn9704@gmail.com <yyn9704@gmail.com>
2022-07-30 21:51:19 +0900

    add check of Internet connection on MainActivity

--- app/src/main/java/com/github/yeeun_yun97/toy/mobymovie/ui/activity/MainActivity.kt
+++ app/src/main/java/com/github/yeeun_yun97/toy/mobymovie/ui/activity/MainActivity.kt
@@ -1,10 +1,63 @@
 package com.github.yeeun_yun97.toy.mobymovie.ui.activity
 
+import android.app.AlertDialog
+import android.content.Context
+import android.net.*
+import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.fragment.app.DialogFragment
 import com.github.yeeun_yun97.clone.ynmodule.ui.activity.YnBaseActivity
+import com.github.yeeun_yun97.clone.ynmodule.ui.component.YnConfirmBaseDialogFragment
+import com.github.yeeun_yun97.toy.mobymovie.R
 import com.github.yeeun_yun97.toy.mobymovie.databinding.ActivityMainBinding
 
 class MainActivity : YnBaseActivity<ActivityMainBinding>() {
+    private lateinit var connManager: ConnectivityManager
+    private var confirmDialog: YnConfirmBaseDialogFragment? = null
+
+    private val networkCallBack = object : ConnectivityManager.NetworkCallback() {
+        override fun onLost(network: Network) {
+            showNetworkNotConnectedDialog()
+        }
+    }
+
     override fun viewBindingInflate(inflater: LayoutInflater): ActivityMainBinding =
         ActivityMainBinding.inflate(inflater)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val builder = NetworkRequest.Builder()
+        connManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        connManager.registerNetworkCallback(builder.build(), networkCallBack)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val connected = connManager.activeNetworkInfo?.isConnected == true
+        if (!connected) showNetworkNotConnectedDialog()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        connManager.unregisterNetworkCallback(networkCallBack)
+    }
+
+    private fun showNetworkNotConnectedDialog() {
+        val dialog = YnConfirmBaseDialogFragment(
+            getString(R.string.connectionFailTitle),
+            getString(R.string.connectionFailMessage),
+            ::closeApp
+        )
+        dialog.isCancelable=false
+        dialog.show(supportFragmentManager, getString(R.string.connectionFailTag))
+        this.confirmDialog= dialog
+    }
+
+    private fun closeApp() {
+        this.confirmDialog?.dismiss()
+        this.confirmDialog = null
+        finishAffinity()
+    }
+
+
 }

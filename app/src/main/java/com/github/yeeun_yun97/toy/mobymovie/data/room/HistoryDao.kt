925015e1967d7b785a489499bfedefc20eeabdc0
yyn9704@gmail.com <yyn9704@gmail.com>
2022-07-31 09:26:42 +0900

    edit Dao - use OnConflictStrategy when duplicated keyword

--- app/src/main/java/com/github/yeeun_yun97/toy/mobymovie/data/room/HistoryDao.kt
+++ app/src/main/java/com/github/yeeun_yun97/toy/mobymovie/data/room/HistoryDao.kt
@@ -3,6 +3,7 @@ package com.github.yeeun_yun97.toy.mobymovie.data.room
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.github.yeeun_yun97.toy.mobymovie.data.model.History
 
@@ -14,11 +15,8 @@ interface HistoryDao {
     @Query("SELECT count(*) FROM History")
     suspend fun countHistory():Int
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertHistory(history: History): Long
-
-    @Query("DELETE FROM History WHERE keyword= :keyword")
-    suspend fun deleteDuplicates(keyword: String)
 
     @Query("DELETE FROM History WHERE hid IN (SELECT hid FROM History ORDER BY hid LIMIT :limit)")
     suspend fun deleteOldsByLimit(limit:Int)
